e88c8024c927acdc73ddec8b6a27df9a9c53d234
art.bensone <rtm.zag@gmail.com>
2020-10-19 20:22:40 +0300

    Fixed values ​​of roman numbers that are greater than 10

--- Calculator/src/com/javamentor/calculators/RomanCalculator.java
+++ Calculator/src/com/javamentor/calculators/RomanCalculator.java
@@ -19,6 +19,43 @@ public class RomanCalculator extends AbstractCalculator implements Calculator {
         romanDigits.put(8, "VIII");
         romanDigits.put(9, "IX");
         romanDigits.put(10, "X");
+        romanDigits.put(11, "XI");
+        romanDigits.put(12, "XII");
+        romanDigits.put(13, "XIII");
+        romanDigits.put(14, "XIV");
+        romanDigits.put(15, "XV");
+        romanDigits.put(16, "XVI");
+        romanDigits.put(17, "XVII");
+        romanDigits.put(18, "XVIII");
+        romanDigits.put(19, "XIX");
+        romanDigits.put(20, "XX");
+        
+        romanDigits.put(21, "XXI");
+        romanDigits.put(24, "XXIV");
+        romanDigits.put(27, "XXVII");
+        romanDigits.put(28, "XXVIII");
+        romanDigits.put(32, "XXXII");
+        romanDigits.put(35, "XXXV");
+        romanDigits.put(36, "XXXVI");
+        romanDigits.put(42, "XLII");
+        romanDigits.put(45, "XLV");
+        romanDigits.put(48, "XLVIII");
+        romanDigits.put(49, "XLIX");
+        romanDigits.put(54, "LIV");
+        romanDigits.put(56, "LVI");
+        romanDigits.put(63, "LXIII");
+        romanDigits.put(64, "LXIV");
+        romanDigits.put(72, "LXXII");
+        romanDigits.put(81, "LXXXI");
+        
+        romanDigits.put(30, "XXX");
+        romanDigits.put(40, "XL");
+        romanDigits.put(50, "L");
+        romanDigits.put(60, "LX");
+        romanDigits.put(70, "LXX");
+        romanDigits.put(80, "LXXX");
+        romanDigits.put(90, "XC");
+        romanDigits.put(100, "C");
     }
 
     @Override
